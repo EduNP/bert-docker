4bc5af103bf9b4672dfe20e55def0509d579d734
EduNP <54189084+EduNP@users.noreply.github.com>
2022-05-22 15:05:23 -0300

    [Update] webApi.py

--- Backend/webApi.py
+++ Backend/webApi.py
@@ -1,19 +1,20 @@
 from flask import Flask
+from markupsafe import escape
 import json
 from bertMasked import *
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "hello"
 
 @app.route('/bertMasked/<text>')
 def masked(text):
-    inputText = text
+    inputText = escape(text)
     result = {}
     result['input'] = inputText
     result['prediction'] = predictToken(inputText, 5)
 
     json_result = json.dumps(result)
     return json_result
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
