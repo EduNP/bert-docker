8793981bd0642ee32a1346f08dfd5f2e50bcf0ec
EduNP <54189084+EduNP@users.noreply.github.com>
2022-06-18 00:01:09 -0300

    Fix CORS

--- Backend/webApi.py
+++ Backend/webApi.py
@@ -1,6 +1,7 @@
 from requests import request
 from flask import Flask
 from flask import request
+from flask import make_response
 from markupsafe import escape
 import json
 from bertMasked import *
@@ -15,21 +16,26 @@ def masked():
     result['error'] = ""
 
     inputArg = request.args.get('inputText', default = '*', type = str)
-    print(f"ETNRADA {inputArg} e argumento {request.args}")
 
     if inputArg == "*" or None:
         result['error'] = "Invalid Input"
-        return json.dumps(result)
+        return responseFunc(json.dumps(result))
 
     inputText = escape(inputArg)
     if "[MASK]" not in inputText:
         result['error'] = "Invalid Format"
-        return json.dumps(result)
+        return responseFunc(json.dumps(result))
 
     
     result['input'] = inputText
     result['prediction'] = predictToken(inputText, 5)
-    return json.dumps(result)
+    return responseFunc(json.dumps(result))
+
+def responseFunc(result):
+    response = make_response(result)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    return response
+    
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)  
